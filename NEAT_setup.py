9026b860e5f5d458153592299dbc2e263d6f5825
rubenahrens <54845741+rubenahrens@users.noreply.github.com>
2023-05-04 16:12:12 +0200

    neat

---
+++ NEAT_setup.py
@@ -0,0 +1,10 @@
+"""
+Using NEAT to create a NN.
+
+Must watch before working on it: https://www.youtube.com/watch?v=2f6TmKm7yx0
+
+https://neat-python.readthedocs.io/en/latest/
+
+"""
+
+import NEAT
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project Mario
  The Mario Bros Project
+
+[GITHUB PROJECT](https://neat-python.readthedocs.io/en/latest/installation.html)
+
+Usefull extensions: GitLens

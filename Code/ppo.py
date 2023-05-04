81e3a85d4faff27705b2a17f2ec40f0e62ea84d8
rubenahrens <54845741+rubenahrens@users.noreply.github.com>
2023-05-04 17:19:10 +0200

    ppo progress

--- Code/ppo.py
+++ Code/ppo.py
@@ -1,33 +1,25 @@
-# TODO create a class for the PPO algorithm
+"""
+PPO algorithm
 
-import neat-python as neat
+We will use the PPO algorithm to train the agent to play the game.
+We will use the stable baselines library to implement the PPO algorithm.
+https://www.youtube.com/watch?v=5P7I-xPq8u8
+https://stable-baselines.readthedocs.io/en/master/modules/ppo2.html
+
+"""
 
 class PPO():
-    def __init__(self):
-        self.config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                                    neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                                    'config-feedforward')
-        self.p = neat.Population(self.config)
-        self.p.add_reporter(neat.StdOutReporter(True))
-        stats = neat.StatisticsReporter()
-        self.p.add_reporter(stats)
-        self.p.add_reporter(neat.Checkpointer(5))
-        self.winner = self.p.run(self.eval_genomes, 300)
-        print('\nBest genome:\n{!s}'.format(winner))
-        print('\nOutput:')
-        winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
-        for xi, xo in zip(xor_inputs, xor_outputs):
-            output = winner_net.activate(xi)
-            print("input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
-    
-    def eval_genomes(self, genomes, config):
-        for genome_id, genome in genomes:
-            genome.fitness = 4.0
-            net = neat.nn.FeedForwardNetwork.create(genome, config)
-            for xi, xo in zip(xor_inputs, xor_outputs):
-                output = net.activate(xi)
-                genome.fitness -= (output[0] - xo[0]) ** 2
-                genome.fitness -= (output[1] - xo[1]) ** 2
+    def __init__(self, config):
+    self.config = config
+
+    def get_action(self, observation, theta=None):
+        raise NotImplementedError
+
+    def get_reward(self, env, theta=None, render=False):
+        raise NotImplementedError
+
+    def train(self):
+        raise NotImplementedError
 
 if __name__ == "__main__":
     PPO()

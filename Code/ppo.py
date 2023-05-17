a42b30b02e3388f158e80d7c3c563294c8bf1da5
rubenahrens <rubenahrens@gmail.com>
2023-05-17 11:38:53 +0200

    ppo

--- Code/ppo.py
+++ Code/ppo.py
@@ -8,18 +8,50 @@ https://stable-baselines.readthedocs.io/en/master/modules/ppo2.html
 
 """
 
-class PPO():
-    def __init__(self, config):
-    self.config = config
+import gym
+import numpy as np
+import tensorflow as tf
+from stable_baselines3 import PPO
 
-    def get_action(self, observation, theta=None):
-        raise NotImplementedError
+# class PPO():
+#     def __init__(self, config):
+#     self.config = config
 
-    def get_reward(self, env, theta=None, render=False):
-        raise NotImplementedError
+#     def get_action(self, observation, theta=None):
+#         raise NotImplementedError
 
-    def train(self):
-        raise NotImplementedError
+#     def get_reward(self, env, theta=None, render=False):
+#         raise NotImplementedError
+
+#     def train(self):
+#         raise NotImplementedError
+
+def main():
+    env = gym.make('SuperMarioBros-v0')
+    
+    config = {
+        "population_size":50,
+        "sigma":0.1,
+        "learning_rate":0.000001,
+        "decay":0.999,
+        "num_episode":1000
+    }
+    
+    model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=config["learning_rate"], 
+            n_steps=512) 
+    
+    model.learn(total_timesteps=1000000)
+    
+    env.reset()
+    done = False
+    while not done:
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        print(observation.shape)
+        print(reward)
+        print(done)
+        print(info)
+
 
 if __name__ == "__main__":
-    PPO()
+    main()
